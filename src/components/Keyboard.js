3bfc71099879e9d80ca57cc82e898b441508c7d5
Pearu Pung <pungpearu@gmail.com>
2022-12-27 19:28:30 +0200

    #2 #8 Give points based on simple challenges

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,8 +3,29 @@ import { useState } from "react"
 const Keyboard = () => {
 
     const keyboard = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "OK"]
+    const challenges = [
+        ['2 + 2 = ', 4],
+        ['1 + 3 = ', 4],
+        ['4 + 4 = ', 8],
+        ['5 + 2 = ', 7],
+        ['6 + 2 = ', 8],
+        ['7 + 2 = ', 9],
+        ['2 + 5 = ', 7],
+        ['2 + 4 = ', 6],
+        ['1 + 1 = ', 2],
+        ['3 + 2 = ', 5],
+    ]
 
     const [userInput, setUserInput] = useState("")
+    const [score, setScore] = useState(0)
+
+    const generateChallenge = () => {
+        const challengeIndex = Math.floor(Math.random() * 10)
+        console.log(challengeIndex)
+        return challenges[challengeIndex] 
+    }
+
+    const [challenge, setChallenge] = useState(generateChallenge())
 
     const addNumber = (number) => {
         if (typeof number !== "number") {
@@ -14,13 +35,24 @@ const Keyboard = () => {
     }
 
     const submitAnswer = () => {
+        if (userInput == challenge[1]) {
+            setScore(score + 2)
+        } else {
+            setScore(score - 1)
+        }
+
         setUserInput('')
+        setChallenge(generateChallenge())
     }
+
+    console.log(challenge)
+
 
     return (
         <div>
             <div className="keyboard">
-            <p className="user-input">{userInput}</p>
+            <div className="user-input">Punkte: {score}</div>
+            <div className="user-input">{challenge[0]}{userInput}</div>
 
             {keyboard.map((key, index) => {
             const isOkButton = key !== "OK"
