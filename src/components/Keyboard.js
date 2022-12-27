e9bc958ea07559377b132542331dec157d3646f0
Pearu Pung <pungpearu@gmail.com>
2022-12-27 14:21:01 +0200

    #1 Mock original style & digit input

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,19 +1,33 @@
+import { useState } from "react"
 
 const Keyboard = () => {
 
-    const keyboard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "OK"]
+    const keyboard = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "OK"]
+
+    const [userInput, setUserInput] = useState("")
+
+    const addNumber = (number) => {
+        if (typeof number !== "number") {
+            return
+        }
+        setUserInput(userInput + number)
+    }
 
     return (
-        <div className="keyboard">
-        {keyboard.map((key, index) => (
+        <div>
+            <div className="keyboard">
+            <p className="user-input">{userInput}</p>
+
+            {keyboard.map((key, index) => (
             <div 
             key={index} 
+            onClick={() => {addNumber(key)}}
             className={key !== "OK"? "button" : "ok-button"}>
                 {key}
                 </div>
         ))}
         </div>
-       
+        </div>
     )
 }
 
